6ba7bf24f711ef8dfcfb142c0eb58553c94f27b5
Stutzman <tstutzman@phoenixchildrens.com>
2019-07-03 10:21:48 -0700

    fix form

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,6 +69,8 @@ class HomeIndex extends React.Component {
                             <div className="8u 12u$(small)">
                                 <form method="post" action="#" data-netlify="true" data-netlify-honeypot="bot-field">
                                     <div className="row uniform 50%">
+                                        <input type="hidden" name="bot-field" />
+                                        <input type="hidden" name="form-name" value="contact" />
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
                                         <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
